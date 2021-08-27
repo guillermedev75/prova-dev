610e1c4b40e3d77876303860b536019721192e2a
Guillerme Manhães <50118895+guillermedev75@users.noreply.github.com>
2021-08-27 01:37:19 -0300

    master

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<!-- Onde o controlador php é "importado" para a pagina -->
 <?php require 'backend/controller.php' ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vendarro LTDA</title>
-    <link rel="stylesheet" href="style.css">
-
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+   <title>Vendarro LTDA</title>
 </head>
 
 <body>
@@ -69,5 +65,4 @@
     </div>
 
 </body>
-
 </html>
